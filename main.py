0452725a809e0eff22ae52bca55b71f2264eed24
levdalba <levandalbashvili9@gmail.com>
2023-10-11 10:12:39 +0700

    ball created

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from turtle import Turtle, Screen
-
-
+from ball import Ball
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 screen.listen()
+
+
 
 screen.exitonclick()
